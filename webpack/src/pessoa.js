49c585bde56f517bffd22aeeef0277cd68a0f654
João Paulo <carrijojp@gmail.com>
2022-04-27 16:21:04 -0300

    Final Webpack

---
+++ webpack/src/pessoa.js
@@ -0,0 +1,7 @@
+export default class Pessoa {
+//module.exports = class Pessoa {
+  cumprimentar() {
+    return "Bom dia!";
+  }
+}
+
---
+++ webpack/src/principal.js
@@ -0,0 +1,7 @@
+import Pessoa from "./pessoa";
+import './assets';
+//import './module/moduleA';
+//const Pessoa = require("./pessoa");
+
+const atendente = new Pessoa();
+console.log(atendente.cumprimentar());
